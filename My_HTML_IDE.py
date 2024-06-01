50efd206d17ecd8f0a1eb80a1458cf64d1ffb8f1
Ravi <developerravi8@gmail.com>
2024-06-01 17:24:02 +0530

    Added file saving feature

--- My_HTML_IDE.py
+++ My_HTML_IDE.py
@@ -2,6 +2,7 @@ from tkinter import *
 from PIL import ImageTk, Image
 from tkinter import filedialog
 import os
+from tkinter import messagebox
 
 root = Tk()
 
@@ -11,7 +12,7 @@ root.maxsize(650, 650)
 root.configure(bg="gray87")
 
 open_img = ImageTk.PhotoImage(Image.open("open.png"))
-close_img = ImageTk.PhotoImage(Image.open("save.png"))
+save_img = ImageTk.PhotoImage(Image.open("save.png"))
 exit_img = ImageTk.PhotoImage(Image.open("exit.jpg"))
 
 def openFile():
@@ -31,6 +32,19 @@ def openFile():
     
     text_box.insert(END, code)
     
+def saveFile(): 
+    file_name = file_name_entry.get()
+    text_file = open(file_name + ".html", "w")
+    data = text_box.get(1.0, END)
+    text_file.write(data)
+    file_name_entry.delete(0, END)
+    root.title("HTML IDE")
+    text_box.delete(1.0, END)
+    messagebox.showinfo("Update", "Success")
+    
+def exitIDE():
+    root.destroy()
+
 file_name_label = Label(root, text="File name")
 file_name_label.place(relx=0.28, rely=0.03, anchor=CENTER)
 
@@ -42,6 +56,12 @@ text_box.place(relx=0.5, rely=0.55, anchor=CENTER)
 
 open_file_button = Button(root, image=open_img, text="Open file", command=openFile)
 open_file_button.place(relx=0.07, rely=0.03, anchor=CENTER)
+
+save_file_button = Button(root, image=save_img, text="Save file", command=saveFile)
+save_file_button.place(relx=0.13, rely=0.03, anchor=CENTER)
+
+exit_file_button = Button(root, image=exit_img, text="Exit file", command=exitIDE)
+exit_file_button.place(relx=0.20, rely=0.03, anchor=CENTER)
 
 
 root.mainloop()
